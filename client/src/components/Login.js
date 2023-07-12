0c0d6b747cfee9658219e395a931980c73af0562
Suyash Shendre <67360905+SuyashShendre@users.noreply.github.com>
2023-07-12 16:54:53 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -42,38 +42,33 @@ const Login = () => {
 
     console.log(needhelp);
     return (
-        <div className="">
-            <div>
-                <center> <h1> Already Member </h1>  </center>
-                <span style={{ "float": "right" }}><Link to="/needhelp" style={{ pointerEvents: needhelp ? 'none' : '' }}> Need Help </Link></span>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <div className="container">
+        <div className="container">
+            <div className="card">
+                <div className="text">
+                    <h3>Already Member</h3>
+                    <p style={{ "marginTop": "10px" }}><Link to="/needhelp" style={{ pointerEvents: needhelp ? 'none' : '' }}> Need Help </Link></p>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-text">
+                        <input type="text" placeholder="Enter Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
 
-                    <label>Username : </label>
-                    <input type="text" placeholder="Enter Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-
-                    <div style={{ "disply": "flex" }}>
-                        <label>Password : </label>
+                    </div>
+                    <div className="input-text">
                         <input type={passwordType} placeholder="Enter Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-
                         <i onClick={togglePassword}>
                             {passwordType === "password" ? <i className="fa fa-eye-slash"></i> : <i className="fa fa-eye"></i>}
                         </i>
                     </div>
-
-                    <div style={{ "color": "red", "marginTop": "10px" }}>{error}</div>
-
-                    <button type="submit">Sign In</button>
-
-                    Dont have an account yet <Link to="/register"> Create an account </Link>
-
-                    <Link to="/needhelp" style={{ pointerEvents: needhelp ? '' : 'none' }}> Need Help </Link>
-                </div>
-            </form>
+                    <h6 style={{ "color": "red", "marginTop": "10px", "marginLeft": "10px" }}>{error}</h6>
+                    <div className="buttons">
+                        <button type="submit">Sign in</button>
+                    </div>
+                    <div className="forgot">
+                        <h3>Dont have an account yet. <Link to="/register"> Create an account </Link></h3>
+                    </div>
+                </form>
+            </div>
         </div>
-
-
     )
 }
 
