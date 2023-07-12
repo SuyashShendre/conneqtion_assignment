3503d8bbc97a1599de4d5433b481b14ffc8deb58
Suyash Shendre <67360905+SuyashShendre@users.noreply.github.com>
2023-07-12 17:49:10 +0530

    Update Dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 
 const Dashboard = () => {
+    const [userInfo, setUserInfo] = useState("");
+
     const handleLogout = () => {
         localStorage.removeItem("token");
         // localStorage.clear();
         window.location.replace("/")
     }
+
+    const fetchData = async () => {
+        if (!localStorage.getItem("token")) {
+            localStorage.clear();
+            window.location.replace("/")
+        }
+
+        let { data } = await axios.get("http://localhost:3001/api/v1/auth/current-user", {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`
+            }
+        });
+        setUserInfo(data.user)
+
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
     return (
         <>
-            <div>Dashboard</div>
-            <button type="button" onClick={handleLogout}>Logout</button>
+            <div style={{ "margin": "10px" }}>
+                <button type="button" onClick={handleLogout} style={{ "float": "right", "padding": "5px", "cursor": "pointer" }}>Logout</button>
+                <h2 style={{ "width": "50%" }}>Dashboard</h2>
+            </div>
+            <div style={{ "margin": "30px 10px" }}>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Username</th>
+                    </tr>
+                    <tr>
+                        <td>{userInfo.firstname} {userInfo.lastname}</td>
+                        <td>{userInfo.email}</td>
+                        <td>{userInfo.username}</td>
+                    </tr>
+                </table>
+            </div>
         </>
     )
 }
