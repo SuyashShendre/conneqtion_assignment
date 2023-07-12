2665013d7f97807c78d9d50ca989a1b25f75ac98
Suyash Shendre <67360905+SuyashShendre@users.noreply.github.com>
2023-07-12 16:55:21 +0530

    Update Register.js

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -10,6 +10,15 @@ const Register = () => {
     const [password, setPassword] = useState("")
 
     const [error, setError] = useState("")
+    const [passwordType, setPasswordType] = useState("password");
+
+    const togglePassword = () => {
+        if (passwordType === "password") {
+            setPasswordType("text")
+            return;
+        }
+        setPasswordType("password")
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -30,32 +39,43 @@ const Register = () => {
     }
 
     return (
-        <div className="">
-            <center> <h1> Registration </h1> </center>
-            <form onSubmit={handleSubmit}>
-                <div className="container">
-                    <label>First Name : </label>
-                    <input type="text" placeholder="Enter First Name" name="firstname" value={firstname} onChange={(e) => setFirstName(e.target.value)} required />
-
-                    <label>Last Name : </label>
-                    <input type="text" placeholder="Enter Last Name" name="lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} required />
-
-                    <label>Email : </label>
-                    <input type="text" placeholder="Enter Email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-
-                    <label>Username : </label>
-                    <input type="text" placeholder="Enter Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-
-                    <label>Password : </label>
-                    <input type="password" placeholder="Enter Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-
-                    <div style={{ "color": "red", "marginTop": "10px" }}>{error}</div>
-
-                    <button type="submit">Sign Up</button>
-
-                    Already have an account <Link to="/"> Click Here </Link>
+        <div className="container">
+            <div className="card">
+                <div className="text">
+                    <h3>Registration</h3>
                 </div>
-            </form>
+                <form onSubmit={handleSubmit}>
+                    <div className="input-text">
+                        <input type="text" placeholder="Enter First Name" name="firstname" value={firstname} onChange={(e) => setFirstName(e.target.value)} required />
+
+                    </div>
+                    <div className="input-text">
+                        <input type="text" placeholder="Enter Last Name" name="lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} required />
+
+                    </div>
+                    <div className="input-text">
+                        <input type="email" placeholder="Enter Email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+
+                    </div>
+                    <div className="input-text">
+                        <input type="text" placeholder="Enter Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+
+                    </div>
+                    <div className="input-text">
+                        <input type={passwordType} placeholder="Enter Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+                        <i onClick={togglePassword}>
+                            {passwordType === "password" ? <i className="fa fa-eye-slash"></i> : <i className="fa fa-eye"></i>}
+                        </i>
+                    </div>
+                    <h6 style={{ "color": "red", "marginTop": "10px", "marginLeft": "10px" }}>{error}</h6>
+                    <div className="buttons">
+                        <button type="submit">Sign Up</button>
+                    </div>
+                    <div className="forgot">
+                        <h3>Already have an account <Link to="/"> Click Here </Link></h3>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
